7ca83947b63831201f78af16526cd3df7d40a50f
Chang <Chang@he.com>
2022-07-13 15:42:53 +0800

    second commit

--- src/main/java/com/chang/test/GitTest.java
+++ src/main/java/com/chang/test/GitTest.java
@@ -3,5 +3,6 @@ package com.chang.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,常和！！！");
+        System.out.println("hello,常和！！！1");
     }
 }
