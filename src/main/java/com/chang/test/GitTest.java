d90be60ac6d1de419ec81d3f42269aed056adb90
Chang <Chang@he.com>
2022-07-13 15:46:47 +0800

    third commit

--- src/main/java/com/chang/test/GitTest.java
+++ src/main/java/com/chang/test/GitTest.java
@@ -4,5 +4,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,常和！！！");
         System.out.println("hello,常和！！！1");
+        System.out.println("hello,hot！！！1");
+        System.out.println("hello,hot！！！1");
+        System.out.println("hello,hot！！！2");
+        System.out.println("hello,hot！！！3");
     }
 }
