50b92a7f669f2fd5910098be6153bd2fc4f96894
Chang <Chang@he.com>
2022-07-13 15:48:51 +0800

    four commit

--- src/main/java/com/chang/test/GitTest.java
+++ src/main/java/com/chang/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello,hot！！！1");
         System.out.println("hello,hot！！！2");
         System.out.println("hello,hot！！！3");
+        System.out.println("hello,hot！！！4");
     }
 }
